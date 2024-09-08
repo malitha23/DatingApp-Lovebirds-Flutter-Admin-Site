14eb773491ae58c8bcffa0919f0b38e1f7e042ae
Malitha Sayuranga <lghmalith44@gmail.com>
2024-09-09 02:37:44 +0530

    Merged 'master' into 'main' with unrelated histories

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -11,7 +11,7 @@ import {
   IoMdInformationCircleOutline,
 } from "react-icons/io";
 import avatar from "assets/img/avatars/avatar4.png";
-import { useNavigate } from 'react-router-dom';
+
 
 const Navbar = (props) => {
   const { onOpenSidenav, brandText } = props;
