0bd9d882db95e1ee8f4009d6a3f3c195eb33bc4a
Malitha Sayuranga <lghmalith44@gmail.com>
2024-09-24 17:20:01 +0530

    Update 2024-09-24

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 // src/config.js
-const API_BASE_URL = 'https://lovebird4u.com/api';
-//  const API_BASE_URL = 'http://localhost:3000/api';
+// const API_BASE_URL = 'https://lovebird4u.com/api';
+  const API_BASE_URL = 'http://localhost:3000/api';
 
 export const API_ENDPOINTS = {
   Base_Url: API_BASE_URL,
